f98e2350e4544542bc3559d0137ad52b2f6f9e41
kimha1030 <kimha1030@gmail.com>
2022-08-31 12:25:32 -0500

    Se modifica estilo en titulo principal

---
+++ citas-vete/README.md
@@ -0,0 +1,8 @@
+# Proyecto Citas Veterinario
+
+Esta aplicación permite almacenar el registro de la cita, con informacion de la mascota, propietario, sintomas y fecha.
+
+## Construido con:
+
+- React JS
+- Tailwind CSS
--- citas-vete/src/components/Header.jsx
+++ citas-vete/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="flex justify-center mb-4">
-      <h1 className="text-4xl font-bold w-1/2 text-center">
+      <h1 className="text-4xl font-bold w-full text-center">
         Seguimiento Pacientes{" "}
         <span className="text-emerald-700">Veterinaria</span>{" "}
       </h1>
